83a3f1a5143b9777421fe003bf34970025f1e86f
Raghuvir Singh Gulia <232739625+raghuvirgulia@users.noreply.github.com>
2026-09-19 17:53:13 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,11 +4,16 @@
 module tb;
 
   // TODO: declare the inputs and outputs
-
-  // TODO: instantiate DUT here
-
-  // Waveform dump configuration (DO NOT CHANGE)
+  reg [1:0] t_sel;
+  wire [7:0] t_dout;
   string vcd_file;
+  // TODO: instantiate DUT here
+lut DUT (
+  .sel (t_sel),
+  .dout (t_dout)
+);
+  // Waveform dump configuration (DO NOT CHANGE)
+  
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
@@ -18,10 +23,14 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
-
+    t_sel=2'd0;
+    #5 t_sel=2'd1;
+    #5 t_sel=2'd2;
+    #5 t_sel=2'd3;
+    #5 $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%d | dout=%d", t_sel, t_dout); // change as required
 
 endmodule
